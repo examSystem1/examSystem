9978657cf9099220eae646f23c370cda1a60cfc3
hhive <784043155@qq.com>
2019-01-06 10:17:22 +0800

    editPaper + editTopic

--- untitled/src/resource/ExamSystem.sql
+++ untitled/src/resource/ExamSystem.sql
@@ -15,9 +15,7 @@ INSERT INTO `examsystem`.`course` (`Cno`, `Cname`) VALUES ('2', '操作系统');
 create table CouChoose(
 id int not null auto_increment primary key,
 Cno varchar(255) not null,
-Uno char(8) not null,
-foreign key (Uno) references user(Uno),
-foreign key (Cno) references course(Cno)
+Uno char(8) not null
 );
 
 create table exam(
@@ -27,9 +25,7 @@ Cno varchar(255) not null,
 Uno char(8) not null,
 beginDate date,
 endDate date,
-Etime int,
-foreign key (Uno) references user(Uno),
-foreign key (Cno) references course(Cno)
+Etime varchar(20)
 );
 
 create table  topic(
@@ -37,6 +33,5 @@ id int not null auto_increment primary key,
 Tname varchar(255) not null,
 choice varchar(2555) not null,
 answer  varchar(255) not null,
-Cno varchar(255) not null,
-foreign key (Cno) references course(Cno)
+Cno varchar(255) not null
 );
